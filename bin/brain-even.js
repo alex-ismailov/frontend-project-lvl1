7f4ba37767a514b3437039500d94158e312e7dda
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-09 13:20:46 +0300

    logic of game was moved into brain-games.js changed names of imported packages.

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,48 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import greeting from '../src/index.js';
+import greetAndGetUserName from '../src/index.js';
+import brainEvenGame from '../src/brain-even-game.js';
 
-const getRandomNumber = () => {
-  const MAX = 20;
-  return Math.floor(Math.random() * (MAX + 1));
-};
-const isEven = (n) => n % 2 === 0;
-
-const isRightAnswer = (guess, currQuestNumber) => {
-  switch (guess) {
-    case 'yes':
-      return isEven(currQuestNumber) === true;
-    case 'no':
-      return isEven(currQuestNumber) === false;
-    default:
-      return null;
-  }
-};
-
-/* *********************************************** */
-
-const nameOfUser = greeting();
-
-const brainEvenGame = (userName, amountOfRounds) => {
-  let round = amountOfRounds;
-  console.log('Answer \'yes\' if the number is even, otherwise answer \'no\'');
-  while (round !== 0) {
-    const currQuestNumber = getRandomNumber();
-    console.log(`Question: ${currQuestNumber}`);
-    const answer = readlineSync.question('Answer: ');
-
-    if (isRightAnswer(answer, currQuestNumber)) {
-      console.log('Correct!');
-      round -= 1;
-    } else {
-      console.log(`"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${userName}!`);
-      return false;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-  return true;
-};
-
-/* testing */
+const nameOfUser = greetAndGetUserName();
 brainEvenGame(nameOfUser, 3);
