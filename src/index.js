352441caff5cbab9d78b383c17757c06350e78f8
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-09 13:27:36 +0300

    Changed name of function. Moved greeting into greetAndGetUserName function.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const greeting = () => {
+const greetAndGetUserName = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
 
-export default greeting;
+export default greetAndGetUserName;
