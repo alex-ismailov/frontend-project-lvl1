8aef4067c4c8d398c5db69a3b70d5df7e710972e
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-10 11:35:23 +0300

    added: src/internal-lib.js

---
+++ src/internal-lib.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const getRandomNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isEven = (n) => n % 2 === 0;
+
+const getUserAnswer = () => readlineSync.question('Answer: ');
+
+const isRightAnswer = (guess, rightAnswer) => guess === rightAnswer;
+
+const checkUserAnswer = (userName, userAnswer, rightAnswer) => {
+  if (isRightAnswer(userAnswer, rightAnswer)) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(
+    `"${userAnswer}" is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`,
+  );
+  return false;
+};
+
+
+export {
+  getRandomNumber, isEven, getUserAnswer, isRightAnswer, checkUserAnswer,
+};
