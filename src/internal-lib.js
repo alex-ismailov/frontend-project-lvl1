a5e5655bdcac012cfd413a903ec67666e26ada2c
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-11 20:01:37 +0300

    src/internal-lib.js: refactored

--- src/internal-lib.js
+++ src/internal-lib.js
@@ -1,34 +1,17 @@
 import readlineSync from 'readline-sync';
 
 const getUserName = () => readlineSync.question('May I have your name? ');
-
-const getRandomNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const isEven = (n) => n % 2 === 0;
-
 const getUserAnswer = () => readlineSync.question('Answer: ');
-
 const isRightAnswer = (guess, rightAnswer) => guess === rightAnswer;
-
-
-
-const checkUserAnswer = (userName, userAnswer, rightAnswer) => {
-  if (isRightAnswer(userAnswer, rightAnswer)) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(
-    `"${userAnswer}" is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`,
-  );
-  return false;
-};
 
 const getTaskText = (gameData) => gameData[0];
 const getQuestion = (gameData) => gameData[1];
 const getRightAnswer = (gameData) => gameData[2];
 
+const getRandomNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min;
+const isEven = (n) => n % 2 === 0;
+
 export {
-  getRandomNumber, isEven, getUserAnswer, isRightAnswer,
-  getUserName, checkUserAnswer, getTaskText, getQuestion,
-  getRightAnswer,
+  getUserName, getUserAnswer, isRightAnswer, getTaskText,
+  getQuestion, getRightAnswer, getRandomNumber, isEven,
 };
