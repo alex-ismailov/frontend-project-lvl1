f9c24a653469d7e56621fa22e4021b1a7f6a638c
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-24 13:46:37 +0300

    refactor(games/brain-calc-game.js) remove getOperator function

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -1,6 +1,8 @@
 import { getRandomNumber } from '../utils.js';
 import startGame from '../index.js';
 
+const operatorColl = ['+', '-', '*'];
+
 const calc = (a, operator, b) => {
   switch (operator) {
     case '+':
@@ -14,19 +16,12 @@ const calc = (a, operator, b) => {
   }
 };
 
-const getOperator = () => {
-  const operators = ['+', '-', '*'];
-  const randomIndex = getRandomNumber(0, operators.length - 1);
-
-  return operators[randomIndex];
-};
-
 const taskText = 'What is the result of the expression?';
 
 const getGameData = () => {
   const firstNum = getRandomNumber();
   const secondNum = getRandomNumber();
-  const operator = getOperator();
+  const operator = operatorColl[getRandomNumber(0, operatorColl.length - 1)];
 
   const question = `${firstNum} ${operator} ${secondNum}`;
   const rightAnswer = calc(firstNum, operator, secondNum);
