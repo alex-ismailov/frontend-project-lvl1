b02304c4df4615e4837b28f3bdec74f673e00c42
alex-ismailov <alexander.is.dev@gmail.com>
2020-03-24 18:18:37 +0300

    refactor(games/brain-even-game.js) remove getRightAnswer function

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -3,17 +3,11 @@ import startGame from '../index.js';
 
 const isEven = (n) => n % 2 === 0;
 
-const getRightAnswer = (question) => (
-  isEven(question)
-    ? 'yes'
-    : 'no'
-);
-
 const taskText = 'Answer "yes" if the number is even, otherwise answer "no"';
 
 const getGameData = () => {
   const question = getRandomNumber();
-  const rightAnswer = getRightAnswer(question);
+  const rightAnswer = isEven(question) ? 'yes' : 'no';
 
   return [String(question), rightAnswer];
 };
