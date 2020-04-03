df5a19e5a079774ec9c3ae843a3b88771b5722fa
smilenb <alexander.is.dev@gmail.com>
2020-04-03 22:31:01 +0300

    remove(utils.js) getQuestion, getRightAnswer functions

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,1 @@
-const getQuestion = (gameData) => gameData[0];
-const getRightAnswer = (gameData) => gameData[1];
-
-const getRandomNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-export {
-  getQuestion, getRightAnswer, getRandomNumber,
-};
+export const getRandomNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min;
